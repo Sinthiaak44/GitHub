38b5425aeb2fbb0da8a3baac8bfacd8f7943e240
Lenovo <Lenovo@LAPTOP-NV1L3Q6R.attlocal.net>
2020-10-15 19:08:48 -0500

    First Commit

---
+++ GitHub/src/Abcd.java
@@ -0,0 +1,66 @@
+
+public class Abcd {
+
+	public static void main(String[] args) {
+		
+		
+		String [] words= {"funk", "chunk", "furry", "hurry", "Red"}; 
+		
+		 
+		 
+		//startsWith
+		for(String w : words) {
+			if(w.endsWith("ry"))
+			System.out.println(w +  " ends with ry");
+		
+		
+		}
+		
+		
+		 for (int i=   words.length -1 ; i >=0; i--) {
+	           System.out.println(words[i]);
+	       }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
